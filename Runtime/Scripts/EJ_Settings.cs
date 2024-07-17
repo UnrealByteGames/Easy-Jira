3d4eeccef7d30256d622c9da290c68519b2d56d7
Daniel Pots <daniel@vr-bits.de>
2024-07-17 12:54:51 +0200

    Changed how JiraSettings are found and create (search all resources and if not found, create one in the nearest resource folder or create resource folder)

--- Runtime/Scripts/EJ_Settings.cs
+++ Runtime/Scripts/EJ_Settings.cs
@@ -1,38 +1,65 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+#if UNITY_EDITOR
+using Mono.Cecil;
 using UnityEditor;
+#endif
 
 namespace UnrealByte.EasyJira {
+	public class EJ_Settings {
+		public static JiraSettings asset;
+		public const string defaultSettingsAssetName = "UB.EJSettings.asset";
 
-    public class EJ_Settings {
+		/// <summary>
+		/// Creates the asset to store all the settings.
+		/// </summary>
+		public static void Awake() {
+#if UNITY_EDITOR
+			//Search for Jira Settings
+			var jirasettingsassets = AssetDatabase.FindAssets("t:JiraSettings");
+			if (jirasettingsassets.Length > 0)
+				asset = (JiraSettings)AssetDatabase.LoadAssetAtPath(AssetDatabase.GUIDToAssetPath(jirasettingsassets[0]), typeof(JiraSettings));
+			else
+				asset = CreateJiraObject();
+#else
+			var jiraSettings = Resources.FindObjectsOfTypeAll<JiraSettings>();
+			if (jiraSettings == null || jiraSettings.Length == 0) 
+				Debug.LogError("Jira settings asset not found");
+			else 
+				asset = jiraSettings[0];
+#endif
+		}
 
-        public static JiraSettings asset;
-        
-        /// <summary>
-        /// Creates the asset to store all the settings.
-        /// </summary>
-        public static void Awake() {
-            UnityEngine.Object jiraSettings = Resources.Load("UB.EJSettings");
+#if UNITY_EDITOR
+		/// <summary>
+		/// Creates a new asset to store settings.
+		/// </summary>
+		public static JiraSettings CreateJiraObject() {
+			asset = ScriptableObject.CreateInstance<JiraSettings>();
+			var bestPath = "";
+			//Search for Resources Folder
+			var resourcesFolder = AssetDatabase.FindAssets("Resources t:Folder");
+			//Find shortest ressources asset
+			if (resourcesFolder.Length > 0) {
+				for (int i = 0; i < resourcesFolder.Length; i++) {
+					var resourcePath = AssetDatabase.GUIDToAssetPath(resourcesFolder[i]);
+					if (resourcePath.Length < bestPath.Length || bestPath.Length == 0)
+						bestPath = resourcePath;
+				}
+			} else {
+				AssetDatabase.CreateFolder("Assets", "Resources");
+				bestPath = "Assets/Resources";
+			}
 
-            if (jiraSettings == null) {
-                asset = CreateJiraObject();
-            } else {
-                asset = (JiraSettings)Resources.Load("UB.EJSettings");
-            }
-        }
+			if (string.IsNullOrEmpty(bestPath)) {
+				Debug.LogError("Something went wrong while creating the Jira settings asset. The Settings cannot be saved");
+			} else {
+				AssetDatabase.CreateAsset(asset, bestPath + "/" + defaultSettingsAssetName);
+				AssetDatabase.SaveAssets();
+			}
 
-        /// <summary>
-        /// Creates a new asset to store settings.
-        /// </summary>
-        public static JiraSettings CreateJiraObject() {
 
-            asset = ScriptableObject.CreateInstance<JiraSettings>();
-
-            AssetDatabase.CreateAsset(asset, "Assets/Easy Jira/Resources/UB.EJSettings.asset");
-            AssetDatabase.SaveAssets();
-            return asset;
-        }
-
-    }
+			return asset;
+		}
+#endif
+	}
 }
