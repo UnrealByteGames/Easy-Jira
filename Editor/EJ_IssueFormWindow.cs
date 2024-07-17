396904c06170ff1109ae1a1d82c4d078db2289dd
Daniel Pots <daniel@vr-bits.de>
2024-07-17 13:52:14 +0200

    Reuse of AutocompleteSearchField in Create Issue assignee

--- Editor/EJ_IssueFormWindow.cs
+++ Editor/EJ_IssueFormWindow.cs
@@ -3,18 +3,23 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
 using System;
+using System.Linq;
+using UnityEditor.IMGUI.Controls;
 
 namespace UnrealByte.EasyJira {
 	public class EJ_IssueFormWindow : EditorWindow {
 		float win = Screen.width;
 		List<JIssueType> issueTypes;
 		List<JIssuePriority> issuePriorities;
-		List<JUser> users;
+		public List<JUser> users;
 		public string summary = "";
 		public string description = "";
-		public string user = "";
+		public JUser juser;
 		public int issueType = 0;
 		public int priority = 0;
+
+		[SerializeField]
+		AutocompleteSearchField autocompleteSearchField;
 
 		void Awake() {
 			if (EJ_Settings.asset == null) {
@@ -22,12 +27,41 @@ namespace UnrealByte.EasyJira {
 			}
 
 			this.StartCoroutine(JiraConnect.APIDownloadProjectIssueTypes(EJ_Settings.asset, EJ_Settings.asset.jiraProjectKey, false, value => issueTypes = value));
-			this.StartCoroutine(JiraConnect.APIDownloadPriorities(EJ_Settings.asset, value => issuePriorities = value));
-			this.StartCoroutine(JiraConnect.APIDownloadUsersAssignable(EJ_Settings.asset, "", EJ_Settings.asset.jiraProjectKey, value => users = value));
+			this.StartCoroutine(JiraConnect.APIDownloadPriorities(EJ_Settings.asset, OnPrioritiesDownloaded));
+			this.StartCoroutine(JiraConnect.APIDownloadUsersAssignable(EJ_Settings.asset, "", EJ_Settings.asset.jiraProjectKey, OnUsersDownloaded));
+		}
+
+		private void OnPrioritiesDownloaded(List<JIssuePriority> priorities) {
+			issuePriorities = priorities;
+			priority = issuePriorities.Count / 2;
+		}
+
+		private void OnUsersDownloaded(List<JUser> users) {
+			this.users = users;
+			foreach (var user in users)
+				autocompleteSearchField.AddResult(user.displayName);
 		}
 
 		void OnEnable() {
+			if (autocompleteSearchField == null) autocompleteSearchField = new AutocompleteSearchField();
+			autocompleteSearchField.onInputChanged = OnAssignedInputChanged;
+			autocompleteSearchField.onConfirm = OnConfirm;
+			autocompleteSearchField.searchString = "";
 			this.Awake();
+		}
+
+		private void OnConfirm(string obj) {
+			autocompleteSearchField.ClearResults();
+		}
+
+		void OnAssignedInputChanged(string searchString) {
+			autocompleteSearchField.ClearResults();
+			if (string.IsNullOrEmpty(searchString) || users == null || users.Exists(x => x.displayName.Equals(searchString)))
+				return;
+			foreach (var user in users) {
+				if (user.displayName.ToLower().StartsWith(searchString.ToLower()))
+					autocompleteSearchField.AddResult(user.displayName);
+			}
 		}
 
 		/// <summary>
@@ -59,16 +93,6 @@ namespace UnrealByte.EasyJira {
 				issuePrioritiesList = aux2.ToArray();
 			}
 
-			string[] userList = { };
-			if (users != null) {
-				List<string> aux3 = new List<string>();
-				foreach (JUser u in users) {
-					aux3.Add(u.displayName);
-				}
-
-				userList = aux3.ToArray();
-			}
-
 			win = Screen.width;
 			EditorStyles.boldLabel.wordWrap = true;
 			EditorStyles.label.wordWrap = true;
@@ -93,18 +117,12 @@ namespace UnrealByte.EasyJira {
 			EditorGUILayout.LabelField("Priority", EditorStyles.boldLabel);
 			priority = EditorGUILayout.Popup(priority, issuePrioritiesList);
 
-
+			AutocompleteSearchField searchField = new AutocompleteSearchField();
 			EditorGUILayout.LabelField("User", EditorStyles.boldLabel);
-			user = EditorGUILayout.TextField(user, GUILayout.Width(200));
-			if (user.Length > 2) {
-				foreach (var jUser in userList) {
-					if (jUser.ToLower().StartsWith(user.ToLower())) {
-						user = jUser;
-						break;
-					}
-				}
-			}
-			EditorGUILayout.Space();
+
+
+			autocompleteSearchField.OnGUI();
+			GUILayout.FlexibleSpace();
 			if (GUILayout.Button("Create")) {
 				try {
 					EditorUtility.DisplayProgressBar("Easy Jira", "Sending issue", 5);
@@ -123,13 +141,13 @@ namespace UnrealByte.EasyJira {
 						}
 					}
 
-					foreach (var jUser in users) {
-						if (jUser.displayName == user) {
-							issue.assignee = jUser;
+					foreach (JUser user in users)
+						if (user.displayName == autocompleteSearchField.searchString) {
+							issue.assignee = user;
 							break;
 						}
-					}
-					
+
+
 					this.StartCoroutine(JiraConnect.APICreateIssue(EJ_Settings.asset, issue, value => {
 						EditorUtility.ClearProgressBar();
 						if (value.StartsWith("error")) {
